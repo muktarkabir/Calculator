6553d1f83bbd7f1d7b0083397aca3079d4581655
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-11-13 22:52:29 +0100

    Add variable that receives user input

--- script.js
+++ script.js
@@ -11,16 +11,20 @@ const timesButton = document.querySelector(".times");
 const divideButton = document.querySelector(".divide");
 const equalSignButton = document.querySelector(".equal-sign button");
 
+let theInput = 0;
+
 clearButton.addEventListener("click", () => {
 	display.textContent = 0;
+	theInput = 0;
 });
 
 deleteButton.addEventListener("click", () => {
-	if (display.textContent.length > 1) {
-		display.textContent = display.textContent.slice(0, -1);
-	}
 	if (display.textContent.length == 1) {
 		display.textContent = 0;
+		theInput = 0;
+	} else if (display.textContent.length > 1) {
+		display.textContent = display.textContent.slice(0, -1);
+		theInput = theInput.slice(0, -1);
 	}
 });
 
@@ -29,8 +33,10 @@ numberDivButtons.forEach((button) => {
 		let enteredNum = button.textContent;
 		if (display.textContent == 0 && display.textContent.length == 1) {
 			display.textContent = enteredNum;
+			theInput = enteredNum;
 		} else {
 			display.textContent += enteredNum;
+			theInput += enteredNum;
 		}
 	});
 });
@@ -45,6 +51,7 @@ decimalPointButton.addEventListener("click", () => {
 	if (display.textContent.includes(".")) {
 	} else {
 		display.textContent += decimalPointButton.textContent;
+		theInput += ".";
 	}
 });
 
