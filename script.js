7debe80c0a3e005a241ac00cc75a1545da9c8a2f
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-11-16 13:59:53 +0100

    Disable input and math buttons
    Buttons will be disabled if a division by zero occurs to prevent
    further errors.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ let results = { output: 0, boolean: true };
 let firstNumber = { value: 0, boolean: true };
 let secondNumber = { value: 0, boolean: false };
 let operand = null;
+let buttonsOn = true;
 
 clearButton.addEventListener("click", () => {
 	display.textContent = "0";
@@ -31,24 +32,14 @@ clearButton.addEventListener("click", () => {
 	firstNumber.value = 0;
 	secondNumber.value = 0;
 	operand = null;
+	buttonsOn = true;
 });
 
 deleteButton.addEventListener("click", () => {
-	if (display.textContent.length == 1 && display.textContent !== "0") {
-		display.textContent = "0";
-		theInput = "";
-		theInput2 = "";
-	} else if (display.textContent.length > 1) {
-		display.textContent = display.textContent.slice(0, -1);
-		if (firstNumber.boolean == true) {
-			theInput = theInput.slice(0, -1);
-		} else if (firstNumber.boolean == false) {
-			theInput2 = theInput2.slice(0, -1);
-		}
-	}
 	if (
+		(display.textContent.length == 1 && display.textContent !== "0") ||
 		display.textContent.includes("a") ||
-		(display.textContent.includes("i") && display.textContent.length > 1)
+		display.textContent.includes("i")
 	) {
 		display.textContent = "0";
 		theInput = "";
@@ -60,66 +51,80 @@ deleteButton.addEventListener("click", () => {
 		firstNumber.value = 0;
 		secondNumber.value = 0;
 		operand = null;
+		buttonsOn = true;
+	} else if (display.textContent.length > 1) {
+		display.textContent = display.textContent.slice(0, -1);
+		if (firstNumber.boolean == true) {
+			theInput = theInput.slice(0, -1);
+		} else if (firstNumber.boolean == false) {
+			theInput2 = theInput2.slice(0, -1);
+		}
 	}
 });
 
 numberDivButtons.forEach((button) => {
 	button.addEventListener("click", () => {
-		if (display.textContent.length < 13) {
-			let enteredNumber = button.textContent;
-			if (display.textContent == "0" && display.textContent.length == 1) {
-				display.textContent = enteredNumber;
-				if (firstNumber.boolean == true) {
-					theInput = enteredNumber;
-				} else if (firstNumber.boolean == false) {
-					theInput2 = enteredNumber;
+		if (buttonsOn == true) {
+			if (display.textContent.length < 13) {
+				let enteredNumber = button.textContent;
+				if (display.textContent == "0" && display.textContent.length == 1) {
+					display.textContent = enteredNumber;
+					if (firstNumber.boolean == true) {
+						theInput = enteredNumber;
+					} else if (firstNumber.boolean == false) {
+						theInput2 = enteredNumber;
+					}
+				} else {
+					display.textContent += enteredNumber;
+					if (firstNumber.boolean == true) {
+						theInput += enteredNumber;
+					} else if (firstNumber.boolean == false) {
+						theInput2 += enteredNumber;
+					}
 				}
-			} else {
-				display.textContent += enteredNumber;
-				if (firstNumber.boolean == true) {
-					theInput += enteredNumber;
-				} else if (firstNumber.boolean == false) {
-					theInput2 += enteredNumber;
+				if (firstNumber.boolean == false && secondNumber.boolean == false) {
+					display.textContent = display.textContent.replace(
+						display.textContent,
+						enteredNumber
+					);
+					secondNumber.boolean = true;
 				}
-			}
-			if (firstNumber.boolean == false && secondNumber.boolean == false) {
-				display.textContent = display.textContent.replace(
-					display.textContent,
-					enteredNumber
-				);
-				secondNumber.boolean = true;
 			}
 		}
 	});
 });
 
 zeroButton.addEventListener("click", () => {
-	if (display.textContent == "0" && display.textContent.length == 1) {
-	} else if (display.textContent.length < 13) {
-		display.textContent += "0";
-		if (firstNumber.boolean == true) {
-			theInput += "0";
-		} else if (secondNumber.boolean == true) {
-			theInput2 += "0";
-		}
-		if (firstNumber.boolean == false && secondNumber.boolean == false) {
-			display.textContent = display.textContent.replace(
-				display.textContent,
-				"0"
-			);
-			secondNumber.boolean = true;
+	if (buttonsOn == true) {
+		if (display.textContent == "0" && display.textContent.length == 1) {
+		} else if (display.textContent.length < 13) {
+			display.textContent += "0";
+			if (firstNumber.boolean == true) {
+				theInput += "0";
+			} else if (secondNumber.boolean == true) {
+				theInput2 += "0";
+			}
+			if (firstNumber.boolean == false && secondNumber.boolean == false) {
+				display.textContent = display.textContent.replace(
+					display.textContent,
+					"0"
+				);
+				secondNumber.boolean = true;
+			}
 		}
 	}
 });
 
 decimalPointButton.addEventListener("click", () => {
-	if (display.textContent.includes(".")) {
-	} else if (display.textContent.length < 13) {
-		display.textContent += ".";
-		if (firstNumber.boolean == true) {
-			theInput += ".";
-		} else if (secondNumber.boolean == true) {
-			theInput2 += ".";
+	if (buttonsOn == true) {
+		if (display.textContent.includes(".")) {
+		} else if (display.textContent.length < 13) {
+			display.textContent += ".";
+			if (firstNumber.boolean == true) {
+				theInput += ".";
+			} else if (secondNumber.boolean == true) {
+				theInput2 += ".";
+			}
 		}
 	}
 });
@@ -134,7 +139,7 @@ const division = function (a, b) {
 	if (b != 0) {
 		return a / b;
 	} else {
-		let errors = ["haha", "impossible", "i can't do it"];
+		let errors = ["haha", "impossible", "infinity"];
 		return errors[Math.floor(Math.random() * 3)];
 	}
 };
@@ -146,40 +151,51 @@ function operate(a, operand, b) {
 }
 
 plusButton.addEventListener("click", () => {
-	firstNumber.value = Number(theInput);
-	firstNumber.boolean = false;
-	operand = addition;
+	if (buttonsOn == true) {
+		firstNumber.value = Number(theInput);
+		firstNumber.boolean = false;
+		operand = addition;
+	}
 });
 
 minusButton.addEventListener("click", () => {
-	firstNumber.value = Number(theInput);
-	firstNumber.boolean = false;
-	operand = subtraction;
+	if (buttonsOn == true) {
+		firstNumber.value = Number(theInput);
+		firstNumber.boolean = false;
+		operand = subtraction;
+	}
 });
 
 timesButton.addEventListener("click", () => {
-	firstNumber.value = Number(theInput);
-	firstNumber.boolean = false;
-	operand = multiplication;
+	if (buttonsOn == true) {
+		firstNumber.value = Number(theInput);
+		firstNumber.boolean = false;
+		operand = multiplication;
+	}
 });
 
 divideButton.addEventListener("click", () => {
-	firstNumber.value = Number(theInput);
-	firstNumber.boolean = false;
-	operand = division;
+	if (buttonsOn == true) {
+		firstNumber.value = Number(theInput);
+		firstNumber.boolean = false;
+		operand = division;
+	}
 });
 
 equalSignButton.addEventListener("click", () => {
-	if (firstNumber.boolean == false && secondNumber.boolean == true) {
-		secondNumber.value = Number(theInput2);
-		results.output = operate(firstNumber.value, operand, secondNumber.value);
-		let displayed = results.output.toString();
-		if (displayed.length > 13) {
-			displayed = displayed.slice(0, 13);
-		}
-		display.textContent = displayed;
-		if (typeof results.output == "string") {
-			return (results.output = 0);
+	if (buttonsOn == true) {
+		if (firstNumber.boolean == false && secondNumber.boolean == true) {
+			secondNumber.value = Number(theInput2);
+			results.output = operate(firstNumber.value, operand, secondNumber.value);
+			let displayed = results.output.toString();
+			if (displayed.length > 13) {
+				displayed = displayed.slice(0, 13);
+			}
+			display.textContent = displayed;
+
+			if (displayed.includes("a") || displayed.includes("i")) {
+				buttonsOn = false;
+			}
 		}
 	}
 });
